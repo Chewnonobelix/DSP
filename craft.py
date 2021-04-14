6db5c706ce021cd240e7e4dc42721f5ccc5f6b38
Perso <chewnonobelix@gmail.com>
2021-04-14 16:23:27 +0200

    read json

--- craft.py
+++ craft.py
@@ -7,26 +7,38 @@ Created on Mon Apr 12 14:18:36 2021
 from enum import Enum
 import math
 import copy
+import json
+
 
 globalDict = {}
+prefered = {}
 speed=1
 
+def load():
+    fObj = open('dsp.json',)
+    jdata = json.loads(fObj.read())
+    
+    for key, value in jdata.items():
+        print(key, value)
+        Craft(jData = value)
+    
 def addComponent(component):
     print(component in globalDict)
     if component in globalDict:
         globalDict[component.name].append(component)
     else:
         globalDict[component.name] = [component]
-    print("end", type(globalDict[component.name]))
+
+    prefered[component.name] = 0
     
 class Crafter(Enum):
     Mining = 0
-    Smelter = 1
-    Assembler = 2
-    Chemical = 3
-    Collider = 4
-    Pump = 5
-    Extractor = 6
+    Pump = 1
+    Extractor = 2
+    Smelter = 3
+    Assembler = 4
+    Chemical = 5
+    Collider = 6
     Raffinery = 7
     
 class Craft:
@@ -38,15 +50,23 @@ class Craft:
     crafter = Crafter.Assembler
     time = 1
     facilities = 1
-    
-    def __init__(self, name, crafter, inputs, outputs, time):
-        self.name = name
-        self.crafter = crafter
-        self.inputs = inputs
-        self.outputs = outputs
-        self.time = time
-            
-        for o in outputs:
+                    
+    def __init__(self, name = None, crafter = None, inputs = None, outputs = None, time = None, jData = None):
+        if jData is None:
+            self.name = name
+            self.crafter = crafter
+            self.inputs = inputs
+            self.outputs = outputs
+            self.time = time
+        else: 
+            self.name = jData["Name"]
+            self.crafter = jData["Crafter"]
+            self.inputs = jData["Input"]
+            self.outputs = jData["Output"]
+            self.time = jData["Time"]
+
+        print(jData, self.name, self.inputs, self.outputs)
+        for o in self.outputs:
             addComponent(self)
     
         
@@ -77,31 +97,37 @@ class Craft:
             self.outputsStream[on] = of * self.facilities
             
     def calculate(self):
-        print("Calculate", self.name)
+        #print("Calculate", self.name)
         sins = self.singleInputsStream()
+        ret = [self]
         for ik, iv in sins.items():
-            print("Input stream", ik, iv*self.facilities, "/min", "facilities: ", self.facilities)
-            temp = copy.deepcopy(globalDict[ik][0])
-            temp.setTargetOutput({ik: iv*self.facilities})
-            if temp.crafter != Crafter.Mining:
-                temp.calculate()
+         #   print("Input stream", ik, iv*self.facilities, "/min", "facilities: ", self.facilities, self.crafter)
+            if self.crafter not in [0,1,2]:
+                temp = copy.deepcopy(globalDict[ik][0])
+                temp.setTargetOutput({ik: iv*self.facilities})
+                #ret.append(temp)
+                ret.append(temp.calculate())
+        return ret
             
                 
 if __name__ == "__main__":
-    c = Craft("Iron ore", Crafter.Mining, {"iron": 1}, {"Iron ore": 30}, 60)
-    c2 = Craft("Iron lingot", Crafter.Smelter, {"Iron ore": 1}, {"Iron lingot": 1}, 1)
-    c3 = Craft("Steel", Crafter.Smelter, {"Iron lingot": 3}, {"Steel": 1}, 3)
+    load()
+    #c = Craft("Iron ore", Crafter.Mining, {"iron": 1}, {"Iron ore": 30}, 60)
+    #c2 = Craft("Iron lingot", Crafter.Smelter, {"Iron ore": 1}, {"Iron lingot": 1}, 1)
+    #c3 = Craft("Steel", Crafter.Smelter, {"Iron lingot": 3}, {"Steel": 1}, 3)
 
+    c2 = copy.deepcopy(globalDict["Iron lingot"][0])
+    c3 = copy.deepcopy(globalDict["Steel"][0])
     for i in range(1, 10):
         c2.setFacilities(i)
         print("Iron Lingot " +str(i)+ " facilities:" + str(c2.outputsStream["Iron lingot"]))
 
     for i in range(1, 100):
-        print("--------------------------")
+        #print("--------------------------")
         c3.setTargetOutput({"Steel": i * 100})
-        print("Steel ", str(i*100), "Facilities:", str(c3.facilities), "Output stream", c3.outputsStream["Steel"], "/min")
-        c3.calculate()
-        
+        #print("Steel ", str(i*100), "Facilities:", str(c3.facilities), "Output stream", c3.outputsStream["Steel"], "/min")
+        calc = c3.calculate()
+        #print(calc)                
     for it in globalDict:
         print(it)
     
